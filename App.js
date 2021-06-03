63d47dbb871dad1f8b42e248d666d034e0c84f86
dalwer001 <dalwer.official@gmail.com>
2021-06-04 01:10:56 +0600

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
+// import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';   
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Lets Build Flying Chat</Text>
+        <StatusBar style="auto" />
+      </View>
+    // </NavigationContainer>
   );
 }
 
---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAp0aXlSd5Ghf6UC_Ji58EhNJPFQ0EJ7xg",
+    authDomain: "flyingchat-a1d78.firebaseapp.com",
+    projectId: "flyingchat-a1d78",
+    storageBucket: "flyingchat-a1d78.appspot.com",
+    messagingSenderId: "1056061102535",
+    appId: "1:1056061102535:web:ac828430e7e9a2716f61e2"
+};
