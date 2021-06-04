c13aae843c2b9d51840901942b49acc361a60c5e
dalwer001 <dalwer.official@gmail.com>
2021-06-04 17:08:54 +0600

    add all

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
-import { ListItem, Avatar} from "react-native-elements";
+import { ListItem, Avatar } from "react-native-elements";
 
-const CustomListItem = ({id, chatName, enterChat}) => {
+const CustomListItem = ({ id, chatName, enterChat }) => {
     return (
         <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
             <Avatar
-             rounded
-             source={{
-                 uri:
-                 "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
-             }}
-             />
-             <ListItem.Content>
-                 <ListItem.Title style={{ fontWeight: "800" }}>
-                     {chatName}
-                 </ListItem.Title>
-                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                     abc
+                rounded
+                source={{
+                    url:
+                        "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+                }}
+            />
+            <ListItem.Content>
+                <ListItem.Title style={{ fontWeight: "800" }}>
+                    {chatName}
+                </ListItem.Title>
+                <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
+                    abc
                  </ListItem.Subtitle>
-             </ListItem.Content>
-             </ListItem>
-   );
+            </ListItem.Content>
+        </ListItem>
+    );
 };
 
 export default CustomListItem
